b81a8403b3f89438c25c9732c66ebb23a62b653d
Mittalpethani98 <mitalpethani23@gmail.com>
2025-05-27 22:37:11 -0400

    final  commit

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,8 +8,8 @@ function Education() {
           Loyalist College, 2024 - Present
         </li>
         <li>
-          <strong>High School Diploma</strong><br />
-          ABC School, 2018 - 2020
+          <strong>Bachelor of Computer Applications</strong><br />
+          Sutex Bank College of Computer Applications & Science, 2019 - 2022
         </li>
       </ul>
     </section>
